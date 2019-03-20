3c5a3c9d12f15ca353c28bff1d91d5a17ff1539c
qasamnawaz <qasamnawaz0142@gmail.com>
2019-03-20 20:46:56 +0500

    my commit

--- config/db-config.js
+++ config/db-config.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 
-var db_url = process.env.DB_URL || 'mongodb://localhost:27017/mydb'
+var db_url = "mongo ds117846.mlab.com:17846/mydb -u Qasam Nawaz -p qasamnawaz0304"
 mongoose.connect(db_url, { useNewUrlParser: true })
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'DB connection error:'));
